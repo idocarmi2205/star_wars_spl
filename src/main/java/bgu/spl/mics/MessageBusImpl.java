599f523cbc5e60fd5934bcc31778a62e6667e6e1
ido <idocarmi2205>
2020-12-01 17:49:43 +0200

    added more shit to MessageBusImpl

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -13,77 +13,81 @@ import java.util.concurrent.LinkedBlockingQueue;
  */
 public class MessageBusImpl implements MessageBus {
 
-	Map<MicroService,BlockingQueue<Message>> microServiceQueues;
-	Map<Class<? extends Event>,BlockingQueue<MicroService>> eventQueues;
-	Map<Class<? extends Event>,List<MicroService>> broadcastLists;
+    Map<MicroService, BlockingQueue<Message>> microServiceQueues;
+    Map<Class<? extends Event>, BlockingQueue<MicroService>> eventQueues;
+    Map<Class<? extends Broadcast>, List<MicroService>> broadcastLists;
 
-	/**
-	 * private constructor to enable creating a single instance of MessageBusImpl
-	 */
-	private MessageBusImpl(){
-		microServiceQueues=new ConcurrentHashMap<>();
-		eventQueues=new ConcurrentHashMap<>();
-		broadcastLists=new ConcurrentHashMap<>();
-	}
-
-	/**
-	 * @return only instance of MessageBusImpl
-	 */
-	public static MessageBusImpl getInstance(){
-		return MessageBusHolder.instance;
-	}
-
-	@Override
-	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
-		if(!eventQueues.containsKey(type)){
-			eventQueues.put(type,new LinkedBlockingQueue<>());
-		}
-		eventQueues.get(type).add(m);
-	}
-
-	@Override
-	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
-		
+    /**
+     * private constructor to enable creating a single instance of MessageBusImpl
+     */
+    private MessageBusImpl() {
+        microServiceQueues = new ConcurrentHashMap<>();
+        eventQueues = new ConcurrentHashMap<>();
+        broadcastLists = new ConcurrentHashMap<>();
     }
 
-	@Override @SuppressWarnings("unchecked")
-	public <T> void complete(Event<T> e, T result) {
-		
-	}
+    /**
+     * @return only instance of MessageBusImpl
+     */
+    public static MessageBusImpl getInstance() {
+        return MessageBusHolder.instance;
+    }
 
-	@Override
-	public void sendBroadcast(Broadcast b) {
-		
-	}
-
-	
-	@Override
-	public <T> Future<T> sendEvent(Event<T> e) {
-		
-        return null;
-	}
-
-	@Override
-	public void register(MicroService m) {
-		if(!microServiceQueues.containsKey(m)){
-			microServiceQueues.put(m,new LinkedBlockingQueue<>());
+    @Override
+    public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
+		synchronized (type) {
+			eventQueues.putIfAbsent(type, new LinkedBlockingQueue<>());
+			eventQueues.get(type).add(m);
 		}
-	}
+    }
 
-	@Override
-	public void unregister(MicroService m) {
-	}
+    @Override
+    public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
+		synchronized (type) {
+			broadcastLists.putIfAbsent(type, new LinkedList<>());
+			broadcastLists.get(type).add(m);
+		}
+    }
 
-	@Override
-	public Message awaitMessage(MicroService m) throws InterruptedException {
-		
-		return null;
-	}
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> void complete(Event<T> e, T result) {
 
-	/**
-	 * private static class which will hold the single instance of MessageBusImpl
-	 */
-	private static class MessageBusHolder{
-		private static final MessageBusImpl instance=new MessageBusImpl();
-	}
+    }
+
+    @Override
+    public void sendBroadcast(Broadcast b) {
+
+    }
+
+
+    @Override
+    public <T> Future<T> sendEvent(Event<T> e) {
+
+        return null;
+    }
+
+    @Override
+    public void register(MicroService m) {
+        if (!microServiceQueues.containsKey(m)) {
+            microServiceQueues.put(m, new LinkedBlockingQueue<>());
+        }
+    }
+
+    @Override
+    public void unregister(MicroService m) {
+    }
+
+    @Override
+    public Message awaitMessage(MicroService m) throws InterruptedException {
+
+        return null;
+    }
+
+    /**
+     * private static class which will hold the single instance of MessageBusImpl
+     */
+    private static class MessageBusHolder {
+        private static final MessageBusImpl instance = new MessageBusImpl();
+    }
 }
