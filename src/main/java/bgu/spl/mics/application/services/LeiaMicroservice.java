8dab04b7d2cb892e340c62fa308c68ec9b7bc0e9
ido <idocarmi2205>
2020-12-06 02:39:40 +0200

    deleted AttackCompleteEvent.java because i understood that it is useless also made leia way better

--- src/main/java/bgu/spl/mics/application/services/LeiaMicroservice.java
+++ src/main/java/bgu/spl/mics/application/services/LeiaMicroservice.java
@@ -1,59 +1,62 @@
 package bgu.spl.mics.application.services;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import bgu.spl.mics.Future;
 import bgu.spl.mics.MicroService;
-import bgu.spl.mics.application.messages.AttackCompleteEvent;
 import bgu.spl.mics.application.messages.AttackEvent;
-import bgu.spl.mics.application.messages.DeactivationEvent;
 import bgu.spl.mics.application.passiveObjects.Attack;
 
 /**
  * LeiaMicroservices Initialized with Attack objects, and sends them as  {@link AttackEvent}.
  * This class may not hold references for objects which it is not responsible for:
  * {@link AttackEvent}.
- *
+ * <p>
  * You can add private fields and public methods to this class.
  * You MAY change constructor signatures and even add new public constructors.
  */
 public class LeiaMicroservice extends MicroService {
 
-	private Map<Attack,Boolean> attacks;
-	
+    private Map<Attack, Future<Boolean>> attacks;
+
+
     public LeiaMicroservice(Attack[] attacks) {
         super("Leia");
 
-		this.attacks=new HashMap<Attack,Boolean>();
-		for (Attack attack:attacks)
-        {
+        this.attacks = new HashMap<>();
+        for (Attack attack : attacks) {
             //used put if absent if the fuckers send us 2 of the same attack
-            this.attacks.putIfAbsent(attack,false);
+            this.attacks.putIfAbsent(attack, null);
         }
     }
 
     @Override
     protected void initialize() {
-        subscribeEvent(AttackCompleteEvent.class,callback->{
-            attacks.put(callback.getAttack().getAttack(),true);
-    });
 
-        attacks.forEach((attack,completed)->{
-            AttackEvent event=new AttackEvent(attack);
-            sendEvent(event);
+        attacks.forEach((attack, completed) -> {
+            AttackEvent event = new AttackEvent(attack);
+            attacks.put(attack, sendEvent(event));
         });
 
-        while(attacks.containsValue(false)){
+        while(!areAllAttacksComplete()){
             try {
                 TimeUnit.MILLISECONDS.sleep(1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+    }
 
-        sendEvent(new DeactivationEvent());
+    private boolean areAllAttacksComplete() {
+        boolean toReturn = true;
+        for (Future<Boolean> future : attacks.values()) {
+            if (!future.isDone()) {
+                toReturn = false;
+                break;
+            }
+        }
+        return toReturn;
     }
 }
